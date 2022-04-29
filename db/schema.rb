d842d52c5ebee72ee7437fc05b47e0c183cdb724
nicho1991 <ladefoged1991@gmail.com>
2022-04-29 18:42:43 +0200

    forgot description field

--- db/schema.rb
+++ db/schema.rb
@@ -17,6 +17,7 @@ ActiveRecord::Schema.define(version: 2022_04_29_162638) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
+    t.text "description"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
